c380ff2dbfe00719b405191e6d50acf9d9126547
Jaykhum <Tchillo83@yahoo.de>
2014-04-25 13:19:40 +0200

    2.korrektur

---
+++ dicewars/src/main/scala/dicewars.scala
@@ -0,0 +1,79 @@
+package main.scala
+import main.scala.controller.DicewarController
+import main.scala.view.TUI
+import main.scala.util.FileUtil
+import java.io.File
+import scala.collection.mutable.ArrayBuffer
+import main.scala.model.Gamefield
+import main.scala.model.Avatar
+
+// tests
+import main.scala.model.Dice
+
+object dicewars {
+    def main(args: Array[String]) {
+    val controller=new DicewarController()
+     
+    
+      
+     /*
+     * Test
+     * */
+/*
+     var attackDice = new Array[Int](10)
+      var dice = new Dice
+	  for(i <- 0 to attackDice.length -1)
+	  {
+	       attackDice(i) = dice.roll
+	       println(i + ": " + attackDice(i))
+	  }
+ */
+     
+      
+      
+//    val game =  new Gamefield
+//    game.initMap
+//    val tui= new TUI(controller, game)
+//    tui.showMenu
+//	while(true)
+//	{
+//		tui.processInputLine(readLine())
+//	}
+
+
+     //val file =  new File("C:\\study\\workspace\\dicewar\\dicewars\\Maps\\")
+    
+//    var player1 = new Avatar
+//    player1.setId(1)
+//    game.mapPosition("basicland");
+//    
+//    game.tactic(player1, game.world(4)(9), game.world(4)(10))
+//    game.tactic(player1, game.world(4)(10), game.world(4)(11))
+//    game.tactic(player1, game.world(4)(9), game.world(5)(9))
+//    game.tactic(player1, game.world(3)(5), game.world(4)(9))
+//    game.tactic(player1, game.world(7)(15), game.world(4)(9))
+
+    // linker n
+//    print("4,10 und 4,9:  ")
+//    game.singleAttack(game.world(4)(10), game.world(4)(9))
+//    // rechts
+//    print("4,10 und 4,11:  ")
+//    game.singleAttack(game.world(4)(10), game.world(4)(11))
+//    
+//    print("4,5 und 4,9:  ")
+//    game.singleAttack(game.world(4)(5), game.world(4)(9))
+//    print("3,5 und 4,9:  ")
+//    game.singleAttack(game.world(3)(5), game.world(4)(9))
+//    // unten n
+//    print("3,5 und 4,5:  ")
+//    game.singleAttack(game.world(3)(5), game.world(4)(5))
+//    // oben n
+//    print("5,5 und 4,5:  ")
+//    game.singleAttack(game.world(5)(5), game.world(4)(5))
+//    var p1 = new Avatar
+//    p1.setId(1)
+//    
+//    game.reinforcement(p1)
+//    tui.showTUI
+  }
+}
---
+++ dicewars/src/main/scala/util/FileUtil.scala
@@ -0,0 +1,15 @@
+package main.scala.util
+
+import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
+
+class FileUtil {
+
+	def readData(filename: String) =  {
+		var inputArray = new ArrayBuffer[String]()
+		for (line <-  Source.fromFile(filename).getLines()) {
+		  inputArray +=line
+		}
+		inputArray
+	}
+}
